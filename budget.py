0605ae05680fc8d12702c78dc54d0984662d136a
amelia6k <ameliakennerly@gmail.com>
2023-11-20 11:24:38 -0600

    coded all

--- budget.py
+++ budget.py
@@ -1,10 +1,10 @@
 # Make planning for your Thanksgiving dinner easy! Log your expenses here!
 
-needed = input("Would you like budgeting assistance? (Y/N): ")
-my_string = ""
 
-while needed == "Y":
-    print("Great! Lets get started")
+my_string = ""
+spending = 0
+while True:
+    print("Lets get started!")
     num_cat = int(input("How many categories of expenses would you like to budget?: "))
     for i in range(num_cat):
         cat_name = input("Enter a category: ")
@@ -12,4 +12,16 @@ while needed == "Y":
         for i in range(num_items):
             item_name = input("Enter an item name: ")
             item_price = float(input("And the price?: $"))
-            my_string = my_string + item_name
+            my_string = my_string + " " + item_name + ","
+            spending = spending + item_price
+        print("Items so far: " + str(my_string))
+    print("In total, you have " + str(num_cat) + " categories.")
+    print("Your items are " + str(my_string))
+    print("Your total spending is " + str(spending))
+    needed = input("Would you like to continue budgeting assistance? (Y/N/IDK):")
+    if needed == "N":
+        break
+    if needed == "IDK":
+        print("You can never budget too much!")
+        continue
+print("Have a great Thanksgiving!")
